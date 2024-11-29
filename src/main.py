02a0ca0358637660c8dc6739eea11b9dc5f1bd9a
alexjohnson <alexjorjohnson3@gmail.com>
2024-11-29 18:04:43 -0500

    Updated frequency computations

---
+++ src/main.py
@@ -0,0 +1,20 @@
+# main.py
+
+import tkinter as tk
+from main import set_gui
+
+
+def main():
+    print("Begin Program...")
+
+    # GUI base setup
+    root = tk.Tk()
+    root.title("Sound Wave Analysis")
+    root.geometry("1400x800")
+
+    set_gui(root)
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
